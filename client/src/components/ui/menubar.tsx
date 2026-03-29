5a82abc284b88b8d7f92a52e77110c554555396e
thanieshkrishna <curiouskiddoo@gmail.com>
2026-03-29 19:38:15 -0400

    Add client/src/components/ui/menubar.tsx

---
+++ client/src/components/ui/menubar.tsx
@@ -0,0 +1,2 @@
+import * as React from "react"
+import { ComponentProps, menubar, MenubarMenu, MenubarMenuContent, MenubarMenuItem, MenubarMenuSeparator, MenubarMenuCheckboxItem, MenubarMenuRadioItem, MenubarMenuTrigger, MenubarMenuSub } from "@/components/ui/menubar"
