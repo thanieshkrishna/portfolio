dbe6966de7be5b51586264f1614e02a8c1e4ef83
thanieshkrishna <curiouskiddoo@gmail.com>
2026-03-29 19:38:13 -0400

    Add client/src/pages/photography.tsx

---
+++ client/src/pages/photography.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { type FC } from "react"
+import Image from "next/image"
+import { ExternalS } from "lucide-react"
+import Button from "@/components/ui/button"
+
+import { Heading, Paragraph } from "@/components/ui/typography"
+import { Container } from "@/components/ui/container"
+
+const Photography: FC = () => {
+  return (
+    <Container>
+      <div className="py-8 px-4">
+        <Heading>Photography</Heading>
+      </div>
+    </Container>
+  
+  
