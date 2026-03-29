fe0095aaf7623de00d7723f567140567d68f52cb
thanieshkrishna <curiouskiddoo@gmail.com>
2026-03-29 19:27:51 -0400

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react",
+import tailwindcss from "@tailwindcss/vite",
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+      "@shared": path.resolve(import.meta.dirname, "shared"),
+    },
+  },
+  root: path.resolve(import.meta.dirname, "client"),
+  build: {
+    outDir: path.resolve(import.meta.dirname, "dist"),
+    emptyOutDir: true,
+  },
+});
