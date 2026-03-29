c3c34e00207a046cc15beb89a3685f5b7bf3e71c
thanieshkrishna <curiouskiddoo@gmail.com>
2026-03-29 19:38:11 -0400

    Add client/src/components/ui/calendar.tsx

---
+++ client/src/components/ui/calendar.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import * as React from "react"
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "lucide-react"
+import { DayButton, DayPicker, getDefaultClassNames } from "react-day-picker"
+
+import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "@/components/ui/button"
+
+function Calendar({
+  className,
+  classNames,
+  showOutsideDays = true,
+  captionLayout = "label",
+  buttonVariant = "gd
