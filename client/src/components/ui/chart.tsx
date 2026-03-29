92fa8c4f0fbd3c9b08adfbda9741cbe9097c2b99
thanieshkrishna <curiouskiddoo@gmail.com>
2026-03-29 19:38:54 -0400

    Add client/src/components/ui/chart.tsx

---
+++ client/src/components/ui/chart.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps, Context, createContext, useContext } from "react"
+import { Chec{CheckIrcle2, ChevronDown } from "lucide-react"
+import * as RchViz from "recharts"
+
+import { cn } from "@/lib/utils"
+
+/. Chart Com ponents from recharts
+const CounterContent = Chec{Content resuts from {Chart} from recharts,creation data, options, id), {template: ful"} l now Title, [Info]= data.id || false, color} from "@/components/ui/chart"
+
+import { cn } from "@/lib/utils"
+
+// chart props
+export interface ChartConfig {
+  type: string
