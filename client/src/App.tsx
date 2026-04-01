9eb1474331a3549df89f97981ff8d7140b827e2c
thanieshkrishna <curiouskiddoo@gmail.com>
2026-04-01 00:42:55 -0400

    Fix App.tsx corrupted content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,42 @@
-import { BrowserRouter, Route, Routes, RouterProvider } from 'react-router-dom'
-import App from './pages/rich pattern with your conpatible style or layouts.'
-    // CIRCULAR: avoid graphiq, hover, or filler screens for `index.mjasl(` around stakles.
-    // THANIESH: é½€«£Í(
-}}
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import Photography from "@/pages/photography";
+import ImageManipulation from "@/pages/image-manipulation";
 
-export default function App() {
-  const routes = createRoutesFromElements([
-    <Route path="/" element={<Layout></Layout>} />,
-    <Route path="*" element={<NotFound></NotFound>} />,
-  ])
-
-  return <RouterProvider router={router} />
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home}/>
+      <Route path="/photography" component={Photography}/>
+      <Route path="/image-manipulation" component={ImageManipulation}/>
+      <Route component={NotFound} />
+    </Switch>
+  );
 }
+
+import { BackgroundCollage } from "@/components/layout/background-collage";
+import { CustomCursor } from "@/components/ui/custom-cursor";
+import { Preloader } from "@/components/ui/preloader";
+import { ScrollProgress } from "@/components/ui/scroll-progress";
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Preloader />
+        <CustomCursor />
+        <ScrollProgress />
+        <BackgroundCollage />
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
